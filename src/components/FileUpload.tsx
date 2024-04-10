fd12b132441f6247868ef721b01231aa1cea073d
Bishwas Pandey <pandeybishwas5@gmail.com>
2024-04-10 22:35:28 +1000

    inital commit

---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+import {useDropzone} from 'react-dropzone'
+
+
+const FileUpload = () => {
+  return (
+    <div className='p-2 bg-white rounded-xl'></div>
+  )
+}
+
+export default FileUpload
